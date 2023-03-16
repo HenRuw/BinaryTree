318305608411ba22a3c2701c15ab594638875fb8
henning.ruwwe <henning4454@gmail.com>
2023-03-16 12:21:49 +0100

    methods completed

--- src/Benutzerverwaltung.java
+++ src/Benutzerverwaltung.java
@@ -1,12 +1,14 @@
 public class Benutzerverwaltung {
     BinarySearchTree<Benutzerprofil> benutzerBaum;
     public void neuenNutzerAnlegen(String pBenutzername, String pPw){
-
+        Benutzerprofil neu = new Benutzerprofil(pBenutzername,pPw);
     }
-    public void nutzerLoeschen(String pBenutzername, String pPw){
-
+    public void nutzerLoeschen(String pBenutzername){
+        Benutzerprofil search = new Benutzerprofil(pBenutzername, "");
+        benutzerBaum.remove(search);
     }
     public Boolean profilVorhanden(String pBenutzername){
-        return benutzerBaum.search(pBenutzername);
+        Benutzerprofil search = new Benutzerprofil(pBenutzername, "");
+        return benutzerBaum.search(search) != null;
     }
 }
